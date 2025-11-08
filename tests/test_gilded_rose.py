acb5726b2860a8f78c88d1a8b821c2253e2d886f
ovidiu-costache <ovidiu.costache2110@stud.acs.upb.ro>
2025-11-08 11:52:27 +0200

    Added useful tests

--- tests/test_gilded_rose.py
+++ tests/test_gilded_rose.py
@@ -11,6 +11,31 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEqual("foo", items[0].name)
 
+    def test_normal_item_degrades(self):
+        items = [Item("Normal Item", 10, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(19, items[0].quality)
+        self.assertEqual(9, items[0].sell_in)
+    
+    def test_quality_never_negative(self):
+        items = [Item("Normal Item", 5, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(0, items[0].quality)
         
+    def test_quality_never_above_50(self):
+        items = [Item("Aged Brie", 10, 50)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(50, items[0].quality)
+        
+    def test_sulfuras_never_changes(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 0, 80)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(80, items[0].quality)
+        self.assertEqual(0, items[0].sell_in)
+
 if __name__ == '__main__':
     unittest.main()
